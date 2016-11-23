26f2f40eaec0b6fc3732ee09d7ba33bc766bcbaf
Román Martínez <roman.mx3@gmail.com>
2016-11-23 21:32:07 +1100

    Init plugin

---
+++ locations-search.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Locations Search
+Plugin URI:
+Description: Create and search locations by keyword, postcode and categories
+Author: Roman Martinez
+Author URI: http://romanmartinez.me/
+Version: 0.1.1
+*/
+
+// No direct access
+defined( 'ABSPATH' ) or die( 'No direct access' );
+define( 'LOCATIONSSEARCHVERSION', '0.1.0' );
