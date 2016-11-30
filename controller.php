7aee4748bb1f301e0465861b3697b7da386d33f5
Román Martínez <roman.mx3@gmail.com>
2016-11-30 23:46:01 +1100

    Setup "lsajax_search_results" ajax function

--- controller.php
+++ controller.php
@@ -17,6 +17,8 @@ if( !class_exists( 'LocationsSearchController' ) ) {
 		static public function init() {
 			add_action( 'wp_enqueue_scripts', array( get_class(), 'frontend_scripts' ) );
 			add_shortcode( 'locations_search', array( get_class(), 'get_locations_search_shortcode' ) );
+			add_action( 'wp_ajax_'.'lsajax_search_results', array( get_class(), 'lsajax_search_results' ) );
+			add_action( 'wp_ajax_nopriv_'.'lsajax_search_results', array( get_class(), 'lsajax_search_results' ) );
 		}
 		
 		
@@ -27,6 +29,7 @@ if( !class_exists( 'LocationsSearchController' ) ) {
 			wp_enqueue_script( 'locations-search', LocationsSearch::get_url().'/js/locations-search.js', array( 'locations-search-google-maps-api', 'jquery', ), LOCATIONSSEARCHVERSION );
 			wp_localize_script( 'locations-search', 'locations_search', array(
 				'ajax_url' => admin_url( 'admin-ajax.php' ),
+				'ajax_action' => 'lsajax_search_results',
 				'text_did_you_mean' => 'Did you mean:',
 			) );
 			wp_enqueue_style( 'locations-search', LocationsSearch::get_url().'/css/locations-search.css', array(), LOCATIONSSEARCHVERSION );
@@ -37,6 +40,25 @@ if( !class_exists( 'LocationsSearchController' ) ) {
 		// ------------------------------
 		static public function get_locations_search_shortcode( $atts=array(), $content='' ) {
 			return LocationsSearchViews::get_container( $atts, $content );
+		}
+		
+		
+		// AJAX Calls
+		// ------------------------------
+		static public function lsajax_search_results() {
+			$posts = LocationsSearchModel::get_closest_locations_from_array( $_POST );
+			foreach( $posts as $i => $post ) {
+				$_post = new stdClass;
+				$_post->lat = $post->lat;
+				$_post->lng = $post->lng;
+				$_post->distance = $post->distance;
+				$_post->distance_units = $post->distance_units;
+				$_post->results_html = LocationsSearchViews::get_results_html( $post );
+				$posts[ $i ] = $_post;
+			}
+			header( 'Content-type: application/json' );
+			echo json_encode( $posts );
+			die();
 		}
 		
 		
