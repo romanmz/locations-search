6fc6101968f5b9985881adaa4d5341404fb233c2
Román Martínez <roman.mx3@gmail.com>
2016-11-30 21:50:31 +1100

    Added [locations_search] shortcode

--- controller.php
+++ controller.php
@@ -15,6 +15,14 @@ if( !class_exists( 'LocationsSearchController' ) ) {
 		// Init
 		// ------------------------------
 		static public function init() {
+			add_shortcode( 'locations_search', array( get_class(), 'get_locations_search_shortcode' ) );
+		}
+		
+		
+		// Shortcodes
+		// ------------------------------
+		static public function get_locations_search_shortcode( $atts=array(), $content='' ) {
+			return LocationsSearchViews::get_container( $atts, $content );
 		}
 		
 		
