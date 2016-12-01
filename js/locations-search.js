6a01b43f025857cfe53ac5c534e55d706496e3c4
Román Martínez <roman.mx3@gmail.com>
2016-12-01 20:27:48 +1100

    Init js variables

--- js/locations-search.js
+++ js/locations-search.js
@@ -1,4 +1,26 @@
 jQuery(document).ready(function($){
 	
 	
+	// Init Vars
+	// ------------------------------
+	
+	// Select objects
+	var container = $('.lsform__container');
+	var searchForm = $('.lsform');
+	var searchOptions = $('.lsform__options');
+	var searchSummary = $('.lsform__summary');
+	var searchResults = $('.lsform__results');
+	var map = $('.lsform__map');
+	
+	// Select fields
+	var fieldQuery = $('#lsform__query');
+	var fieldDistance = $('#lsform__distance');
+	var fieldDistanceUnits = $('#lsform__distanceunits');
+	
+	// No scripts necessary if there's no search form, or a place to display the results
+	if( !searchForm.length || ( !searchResults.length && !map.length ) ) {
+		return;
+	}
+	
+	
 });
