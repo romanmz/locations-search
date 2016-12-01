fbce0e5d3420c38811b141faaa6a7a29deb4dfaf
Román Martínez <roman.mx3@gmail.com>
2016-12-01 22:18:10 +1100

    Added js form functions

--- js/locations-search.js
+++ js/locations-search.js
@@ -27,4 +27,69 @@ jQuery(document).ready(function($){
 	var googleMap = map.length ? new google.maps.Map( map[0], { zoom:15, } ) : false;
 	
 	
+	// Form functions
+	// ------------------------------
+	var formLock = function() {
+		searchForm.data( 'isLocked', true );
+		container.addClass( 'lsform--loading' );
+	}
+	var formUnlock = function() {
+		searchForm.data( 'isLocked', false );
+		container.removeClass( 'lsform--loading' );
+	}
+	var formShowOptions = function( geocodeResults ) {
+		
+		// Unlock form and clean options
+		formUnlock();
+		searchOptions.empty();
+		if( !geocodeResults.length ) {
+			return;
+		}
+		
+		// Create list
+		var list = $('<ul>');
+		$.each( geocodeResults, function( i, geocodeResult ){
+			var item = $('<li>');
+			var link = $('<a>',{ href:'#', text:geocodeResult.formatted_address });
+			link.on( 'click', function(e){
+				e.preventDefault();
+				locationsGetByGeocode( geocodeResult );
+			});
+			list.append( item.append( link ) );
+		});
+		
+		// Update options
+		searchOptions.html( '<div>'+locations_search.text_did_you_mean+'</div>' );
+		searchOptions.append( list );
+	}
+	var formShowSummary = function( locations ) {
+		if( !locations.length ) {
+			searchSummary.html( '<p>'+locations_search.text_0_results+'</p>' );
+		} else if( locations.length == 1 ) {
+			searchSummary.html( '<p>'+locations_search.text_1_result+'</p>' );
+		} else {
+			searchSummary.html( '<p>'+locations_search.text_more_results.replace( '#', locations.length ) +'</p>' );
+		}
+	}
+	var formShowResults = function( locations ) {
+		
+		// Unlock form and clean results
+		formUnlock();
+		searchResults.empty();
+		if( !locations.length ) {
+			return;
+		}
+		
+		// Create list
+		var list = $('<ul>');
+		$.each( locations, function( i, location ) {
+			var item = $('<li>').append( location.results_html );
+			list.append( item );
+		});
+		
+		// Update results
+		searchResults.append( list );
+	}
+	
+	
 });
