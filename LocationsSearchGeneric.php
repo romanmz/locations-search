66ce050e20b781ef51a7f8dafd7451615fd91172
Román Martínez <roman.mx3@gmail.com>
2016-12-08 19:21:50 +1100

    Added "get_language_codes" method

--- LocationsSearchGeneric.php
+++ LocationsSearchGeneric.php
@@ -222,6 +222,62 @@ if( !class_exists( 'LocationsSearchGeneric' ) ) {
 			'zm' => 'Zambia',
 			'zw' => 'Zimbabwe',
 		);
+		// https://developers.google.com/maps/faq#languagesupport
+		static private $language_codes = array(
+			'ar' => 'Arabic',
+			'bg' => 'Bulgarian',
+			'bn' => 'Bengali',
+			'ca' => 'Catalan',
+			'cs' => 'Czech',
+			'da' => 'Danish',
+			'de' => 'German',
+			'el' => 'Greek',
+			'en' => 'English',
+			'en-AU' => 'English (Australian)',
+			'en-GB' => 'English (Great Britain)',
+			'es' => 'Spanish',
+			'eu' => 'Basque',
+			'fa' => 'Farsi',
+			'fi' => 'Finnish',
+			'fil' => 'Filipino',
+			'fr' => 'French',
+			'gl' => 'Galician',
+			'gu' => 'Gujarati',
+			'hi' => 'Hindi',
+			'hr' => 'Croatian',
+			'hu' => 'Hungarian',
+			'id' => 'Indonesian',
+			'it' => 'Italian',
+			'iw' => 'Hebrew',
+			'ja' => 'Japanese',
+			'kn' => 'Kannada',
+			'ko' => 'Korean',
+			'lt' => 'Lithuanian',
+			'lv' => 'Latvian',
+			'ml' => 'Malayalam',
+			'mr' => 'Marathi',
+			'nl' => 'Dutch',
+			'no' => 'Norwegian',
+			'pl' => 'Polish',
+			'pt' => 'Portuguese',
+			'pt-BR' => 'Portuguese (Brazil)',
+			'pt-PT' => 'Portuguese (Portugal)',
+			'ro' => 'Romanian',
+			'ru' => 'Russian',
+			'sk' => 'Slovak',
+			'sl' => 'Slovenian',
+			'sr' => 'Serbian',
+			'sv' => 'Swedish',
+			'ta' => 'Tamil',
+			'te' => 'Telugu',
+			'th' => 'Thai',
+			'tl' => 'Tagalog',
+			'tr' => 'Turkish',
+			'uk' => 'Ukrainian',
+			'vi' => 'Vietnamese',
+			'zh-CN' => 'Chinese (Simplified)',
+			'zh-TW' => 'Chinese (Traditional)',
+		);
 		
 		
 		// Methods
@@ -231,6 +287,9 @@ if( !class_exists( 'LocationsSearchGeneric' ) ) {
 		}
 		static public function get_country_name( $code ) {
 			return isset( self::$country_codes[ $code ] ) ? self::$country_codes[ $code ] : '';
+		}
+		static public function get_language_codes() {
+			return self::$language_codes;
 		}
 		
 		
