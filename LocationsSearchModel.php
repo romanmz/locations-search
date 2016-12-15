1ae40712e299568f14aff066c4c8217e287c87c3
Román Martínez <roman.mx3@gmail.com>
2016-12-15 20:29:57 +1100

    Allow filtering by state on "get_closest_locations_from_array"method

--- LocationsSearchModel.php
+++ LocationsSearchModel.php
@@ -62,6 +62,10 @@ if( !class_exists( 'LocationsSearchModel' ) ) {
 			return $posts;
 			
 		}
+		
+		
+		// Get Closest Locations from Array
+		// ------------------------------
 		static public function get_closest_locations_from_array( $atts=array() ) {
 			
 			// Init vars
@@ -70,11 +74,24 @@ if( !class_exists( 'LocationsSearchModel' ) ) {
 				'lng' => 0,
 				'distance' => 0,
 				'distance_units' => '',
+				'query_type' => '',
+				'query_values' => '',
+				'state' => '',
 				'lcategory' => false,
 			);
 			extract( shortcode_atts( $defaults, $atts ) );
 			$filters = array();
-			$posts = array();
+			$posts = NULL;
+			
+			// Filter: state
+			if( !empty( $state ) ) {
+				$filters['meta_query'] = array();
+				$filters['meta_query'][] = array(
+					'key' => 'state',
+					'value' => $state,
+					'compare' => '=',
+				);
+			}
 			
 			// Filter: location category
 			if( !empty( $lcategory ) ) {
@@ -87,36 +104,61 @@ if( !class_exists( 'LocationsSearchModel' ) ) {
 				);
 			}
 			
-			// Search: Distance + Filters
-			if( $distance && !empty( $filters ) ) {
-				$posts = self::get_closest_locations( $lat, $lng, $distance, $distance_units );
-				if( empty( $posts ) ) {
-					return $posts;
+			// Search by distance
+			if( $lat && $lng && $distance ) {
+				$posts = self::get_closest_ids( $lat, $lng, $distance, $distance_units );
+			}
+			
+			// Include queried areas
+			if( in_array( $query_type, array( 'suburb', 'state', 'postcode', 'country', ) ) && is_array( $query_values ) ) {
+				if( is_null( $posts ) ) {
+					$posts = array();
 				}
-				$post_ids = array_map( function( $post ){ return $post->ID; }, $posts );
-				$filters['post_type'] = 'location';
-				$filters['post__in'] = $post_ids;
-				$filters['orderby'] = 'post__in';
-				$filters['fields'] = 'ids';
-				$filtered_ids = get_posts( $filters );
-				foreach( $posts as $i => $post ) {
-					if( !in_array( $post->ID, $filtered_ids ) ) {
-						unset( $posts[ $i ] );
+				$posts = array_merge( $posts, get_posts( array(
+					'post_type' => 'location',
+					'post_status' => 'publish',
+					'posts_per_page' => -1,
+					'post__not_in' => array_map( function($post){return $post->ID;}, $posts ),
+					'meta_query' => array(
+						array(
+							'key' => $query_type,
+							'value' => array_unique( $query_values ),
+							'compare' => 'IN',
+						),
+					),
+					'orderby' => array( 'title' => 'ASC' ),
+				) ) );
+			}
+			
+			// Use filters
+			if( !empty( $filters ) ) {
+				
+				// To reduce existing selection
+				if( !is_null( $posts ) && !empty( $posts ) ) {
+					$filters['post_type'] = 'location';
+					$filters['post_status'] = 'publish';
+					$filters['posts_per_page'] = -1;
+					$filters['post__in'] = array_map( function($post){return $post->ID;}, $posts );
+					$filters['orderby'] = 'post__in';
+					$filters['fields'] = 'ids';
+					$filtered_ids = get_posts( $filters );
+					foreach( $posts as $i => $post ) {
+						if( !in_array( $post->ID, $filtered_ids ) ) {
+							unset( $posts[ $i ] );
+						}
 					}
+					$posts = array_values( $posts );
 				}
-				$posts = array_values( $posts );
-			}
-			// Search: Distance only
-			elseif( $distance ) {
-				$posts = self::get_closest_locations( $lat, $lng, $distance, $distance_units );
-			}
-			// Search: Filters only
-			elseif( !empty( $filters ) ) {
-				$filters['post_type'] = 'location';
-				$filters['post_status'] = 'publish';
-				$filters['posts_per_page'] = -1;
-				$filters['orderby'] = 'post_title';
-				$posts = get_posts( $filters );
+				
+				// or to create a stand-alone query
+				elseif( is_null( $posts ) ) {
+					$filters['post_type'] = 'location';
+					$filters['post_status'] = 'publish';
+					$filters['posts_per_page'] = -1;
+					$filters['orderby'] = array( 'title' => 'ASC' );
+					$posts = get_posts( $filters );
+				}
+				
 			}
 			
 			// Cast vars
