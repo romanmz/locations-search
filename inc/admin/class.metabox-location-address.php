c7b7165cf896a5638f45a4c7cc68206c1bb1eb9a
Roman Martinez <roman.mx3@gmail.com>
2018-01-22 11:46:05 +1100

    Move duplicated code from meta box classes into a separate helper class

--- inc/admin/class.metabox-location-address.php
+++ inc/admin/class.metabox-location-address.php
@@ -15,7 +15,7 @@ use Locations_Search as NS;
  * @since 1.0.0
  * @todo Support i18n
  */
-class Metabox_Location_Address {
+class Metabox_Location_Address extends Metabox {
 	
 	/**
 	 * @var string|array The name of the post type(s) that should load this metabox
@@ -31,6 +31,7 @@ class Metabox_Location_Address {
 		'title' => 'Location Address',
 		'context' => 'advanced',			// 'advanced'*|'normal'|'side'
 		'priority' => 'default',			// 'default'*|'high'|'low'
+		'file' => 'metabox-location-address.php',
 	];
 	
 	/**
@@ -74,92 +75,6 @@ class Metabox_Location_Address {
 	];
 	
 	/**
-	 * Init function
-	 * 
-	 * Registers the necessary hooks and functions
-	 * 
-	 * @return void
-	 */
-	static public function init() {
-		foreach( self::$fields as $field_name => $field_data ) {
-			self::$fields[ $field_name ] = self::prepare_field_data( $field_name, $field_data );
-		}
-		add_action( 'add_meta_boxes_'.self::$post_type, [__CLASS__, 'register_metabox'] );
-		add_action( 'save_post', [__CLASS__, 'save_post_meta'] );
-		add_action( 'admin_enqueue_scripts', [__CLASS__, 'load_assets'] );
-	}
-	
-	/**
-	 * Fill in default field attributes
-	 * 
-	 * @param string $field_name
-	 * @param array $field_data
-	 * @return array
-	 */
-	static public function prepare_field_data( $field_name, $field_data ) {
-		$default_atts = [
-			'label' => '',
-			'id' => self::$metabox['id'].'_'.$field_name,
-			'type' => 'text',
-			'sanitize' => null,
-		];
-		$field_data = wp_parse_args( $field_data, $default_atts );
-		$field_data['name'] = $field_name;
-		return $field_data;
-	}
-	
-	/**
-	 * Registers the new metabox
-	 * 
-	 * @return void
-	 */
-	static public function register_metabox( $post ) {
-		add_meta_box(
-			self::$metabox['id'],
-			self::$metabox['title'],
-			[__CLASS__, 'render_metabox'],
-			self::$post_type,
-			self::$metabox['context'],
-			self::$metabox['priority']
-		);
-	}
-	
-	/**
-	 * Saves the user data into the database
-	 * 
-	 * @param int $post_id
-	 * @return void
-	 */
-	static public function save_post_meta( $post_id ) {
-		
-		// Validate request
-		$nonce_action = self::$nonce['action'].$post_id;
-		if(
-			get_post_type( $post_id ) !== self::$post_type
-			|| !isset( $_POST[ self::$nonce['name'] ] )
-			|| !wp_verify_nonce( $_POST[ self::$nonce['name'] ], $nonce_action )
-		) {
-			return;
-		}
-		
-		// Loop meta values
-		foreach( self::$fields as $field_name => $field_data ) {
-			
-			// Get new value and sanitize it
-			$new_value = isset( $_POST[ $field_name ] ) ? $_POST[ $field_name ] : '';
-			$sanitize_func = is_callable( $field_data['sanitize'] ) ? $field_data['sanitize'] : 'sanitize_text_field';
-			$new_value = call_user_func( $sanitize_func, $new_value );
-			
-			// Update database
-			if( empty( $new_value ) ) {
-				delete_post_meta( $post_id, $field_name );
-			} else {
-				update_post_meta( $post_id, $field_name, $new_value );
-			}
-		}
-	}
-	
-	/**
 	 * Load the necessary assets for the meta boxes
 	 * 
 	 * @param string $hook
@@ -179,34 +94,6 @@ class Metabox_Location_Address {
 		wp_enqueue_style( NS\PLUGIN_NAME.'_edit-screen', NS\PLUGIN_URL.'assets/css/edit-screen.css', [], NS\PLUGIN_VERSION );
 		wp_enqueue_script( NS\PLUGIN_NAME.'_google-maps-api', '//maps.googleapis.com/maps/api/js?key=' );
 		wp_enqueue_script( NS\PLUGIN_NAME.'_edit-screen', NS\PLUGIN_URL.'assets/js/edit-screen.js', [NS\PLUGIN_NAME.'_google-maps-api', 'jquery'], NS\PLUGIN_VERSION );
-	}
-	
-	/**
-	 * Outputs the HTML of the meta box
-	 * 
-	 * @param WP_Post $post
-	 * @return void
-	 */
-	static function render_metabox( $post ) {
-		$nonce_action = self::$nonce['action'].$post->ID;
-		wp_nonce_field( $nonce_action, self::$nonce['name'] );
-		include( 'views/metabox-location-address.php' );
-	}
-	
-	/**
-	 * Outputs the HTML of an individual meta box field
-	 * 
-	 * @param string $field_name
-	 * @return void
-	 */
-	static function render_field( $field_name ) {
-		if( !isset( self::$fields[ $field_name ] ) ) {
-			return;
-		}
-		global $post;
-		$atts = self::$fields[ $field_name ];
-		$atts['value'] = get_post_meta( $post->ID, $field_name, true );
-		include( 'views/metabox-text-field.php' );
 	}
 	
 }
--- inc/admin/class.metabox-location-details.php
+++ inc/admin/class.metabox-location-details.php
@@ -15,7 +15,7 @@ use Locations_Search as NS;
  * @since 1.0.0
  * @todo Support i18n
  */
-class Metabox_Location_Details {
+class Metabox_Location_Details extends Metabox {
 	
 	/**
 	 * @var string|array The name of the post type(s) that should load this metabox
@@ -31,6 +31,7 @@ class Metabox_Location_Details {
 		'title' => 'Location Details',
 		'context' => 'advanced',			// 'advanced'*|'normal'|'side'
 		'priority' => 'default',			// 'default'*|'high'|'low'
+		'file' => 'metabox-location-details.php',
 	];
 	
 	/**
@@ -61,100 +62,6 @@ class Metabox_Location_Details {
 	];
 	
 	/**
-	 * Init function
-	 * 
-	 * Registers the necessary hooks and functions
-	 * 
-	 * @return void
-	 */
-	static public function init() {
-		foreach( self::$fields as $field_name => $field_data ) {
-			self::$fields[ $field_name ] = self::prepare_field_data( $field_name, $field_data );
-		}
-		add_action( 'add_meta_boxes_'.self::$post_type, [__CLASS__, 'register_metabox'] );
-		add_action( 'save_post', [__CLASS__, 'save_post_meta'] );
-		add_action( 'admin_enqueue_scripts', [__CLASS__, 'load_assets'] );
-	}
-	
-	/**
-	 * Fill in default field attributes
-	 * 
-	 * @param string $field_name
-	 * @param array $field_data
-	 * @return array
-	 */
-	static public function prepare_field_data( $field_name, $field_data ) {
-		$default_atts = [
-			'label' => '',
-			'id' => self::$metabox['id'].'_'.$field_name,
-			'type' => 'text',
-			'sanitize' => null,
-		];
-		$field_data = wp_parse_args( $field_data, $default_atts );
-		$field_data['name'] = $field_name;
-		return $field_data;
-	}
-	
-	/**
-	 * Registers the new metabox
-	 * 
-	 * @return void
-	 */
-	static public function register_metabox( $post ) {
-		add_meta_box(
-			self::$metabox['id'],
-			self::$metabox['title'],
-			[__CLASS__, 'render_metabox'],
-			self::$post_type,
-			self::$metabox['context'],
-			self::$metabox['priority']
-		);
-	}
-	
-	/**
-	 * Saves the user data into the database
-	 * 
-	 * @param int $post_id
-	 * @return void
-	 */
-	static public function save_post_meta( $post_id ) {
-		
-		// Validate request
-		$nonce_action = self::$nonce['action'].$post_id;
-		if(
-			get_post_type( $post_id ) !== self::$post_type
-			|| !isset( $_POST[ self::$nonce['name'] ] )
-			|| !wp_verify_nonce( $_POST[ self::$nonce['name'] ], $nonce_action )
-		) {
-			return;
-		}
-		
-		// Loop meta values
-		foreach( self::$fields as $field_name => $field_data ) {
-			
-			// Get new value and sanitize it
-			$new_value = isset( $_POST[ $field_name ] ) ? $_POST[ $field_name ] : '';
-			$sanitize_func = is_callable( $field_data['sanitize'] ) ? $field_data['sanitize'] : 'sanitize_text_field';
-			if( $field_name == 'opening_hours' ) {
-				if( !is_array( $new_value ) ) {
-					$new_value = false;
-				} else {
-					array_walk_recursive( $new_value, 'sanitize_text_field' );
-				}
-			} else {
-				$new_value = call_user_func( $sanitize_func, $new_value );
-			}
-			
-			// Update database
-			if( empty( $new_value ) ) {
-				delete_post_meta( $post_id, $field_name );
-			} else {
-				update_post_meta( $post_id, $field_name, $new_value );
-			}
-		}
-	}
-	
-	/**
 	 * Load the necessary assets for the meta boxes
 	 * 
 	 * @param string $hook
@@ -172,51 +79,6 @@ class Metabox_Location_Details {
 		
 		// Load assets
 		wp_enqueue_style( NS\PLUGIN_NAME.'_edit-screen', NS\PLUGIN_URL.'assets/css/edit-screen.css', [], NS\PLUGIN_VERSION );
-	}
-	
-	/**
-	 * Outputs the HTML of the meta box
-	 * 
-	 * @param WP_Post $post
-	 * @return void
-	 */
-	static function render_metabox( $post ) {
-		$nonce_action = self::$nonce['action'].$post->ID;
-		wp_nonce_field( $nonce_action, self::$nonce['name'] );
-		include( 'views/metabox-location-details.php' );
-	}
-	
-	/**
-	 * Outputs the HTML of an individual meta box field
-	 * 
-	 * @param string $field_name
-	 * @return void
-	 */
-	static function render_field( $field_name ) {
-		if( !isset( self::$fields[ $field_name ] ) ) {
-			return;
-		}
-		global $post;
-		$atts = self::$fields[ $field_name ];
-		$atts['value'] = get_post_meta( $post->ID, $field_name, true );
-		
-		// Variations for email and website fields
-		// !pre-escaping/validation/value types (array|string|int|etc)
-		// !default values
-		if( $field_name == 'email' ) {
-			$atts['value'] = is_email( $atts['value'] );
-		} elseif( $field_name == 'website' ) {
-			$atts['value'] = esc_url( $atts['value'] );
-		} elseif( $field_name == 'opening_hours' && empty( $atts['value'] ) ) {
-			$atts['value'] = [];
-		}
-		
-		// Load template
-		if( $field_name == 'opening_hours' ) {
-			include( 'views/metabox-opening-hours.php' );
-		} else {
-			include( 'views/metabox-text-field.php' );
-		}
 	}
 	
 }
