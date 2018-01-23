5fe90d68cffc7b7f502e092db6c49335538f4cd6
Roman Martinez <roman.mx3@gmail.com>
2018-01-23 16:15:50 +1100

    add "locations_map_search" shortcode (base code only)

--- assets/js/search-map.js
+++ assets/js/search-map.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
 	
 	
 	// ==================================================
-	// MAP FUNCTIONS
+	// FUNCTIONS
 	// ==================================================
 	
 	// MAPS
@@ -54,9 +54,134 @@ jQuery(document).ready(function($){
 			label: label,
 		});
 	}
+	
+	
+	// GEOCODING
+	// ------------------------------
+	
+	// Submit geocoding query
+	function geocodeQuery( caller, address ) {
+		
+		// Check lock
+		if( caller.data( 'isLocked' ) ) {
+			return;
+		}
+		
+		// Prepare query
+		var query = {};
+		query.address = $.trim( address );
+		query.componentRestrictions = {};
+		
+		// Validate query
+		if( !query.address ) {
+			alert( locsearch.alerts.empty_address );
+			return;
+		}
+		
+		// Send geocode request
+		caller.trigger( 'locsearch_geocode_lock' );
+		geocoder.geocode( query, function( results, status ){
+			caller.trigger( 'locsearch_geocode_unlock' );
+			geocodeReadErrors( results, status );
+			if( status == 'OK' ) {
+				caller.trigger( 'locsearch_geocode_results', [results] );
+				if( !results.length ) {
+					caller.trigger( 'locsearch_geocode_0_results' );
+				} else if( results.length == 1 ) {
+					caller.trigger( 'locsearch_geocode_1_result', results );
+				} else {
+					caller.trigger( 'locsearch_geocode_many_results', [results] );
+				}
+			}
+		});
+	}
+	
+	// Check for errors in received data
+	function geocodeReadErrors( results, status ) {
+		if( status == 'INVALID_REQUEST' ) {
+			alert( locsearch.alerts.invalid_request );
+		}
+		if( status == 'OVER_QUERY_LIMIT' ) {
+			alert( locsearch.alerts.query_limit );
+		}
+		if( status == 'ZERO_RESULTS' || ( status == 'OK' && !results.length ) ) {
+			alert( locsearch.alerts.no_results );
+		}
+		if( status != 'OK' ) {
+			// UNKNOWN_ERROR and REQUEST_DENIED
+			alert( locsearch.alerts.error_unknown );
+		}
+	}
+	
+	// DATABASE QUERIES
+	// ------------------------------
+	
+	
+	
+	// ==================================================
+	// SHORTCODES
+	// ==================================================
+	
+	// Check Google Maps API
+	if( typeof google !== 'object' ) {
+		alert( locsearch.alerts.api_unavailable );
+	}
+	var geocoder = null;
+	
 	// Generate locations maps
 	$('.locsearch_map').each(function(){
 		var map = createMap( this );
 		var markers = addMarkers( map, $(this).data('locations') );
+	});
+	
+	// Initialize search boxes
+	$('.locsearch_box').each(function(){
+		
+		// Init objects
+		var box = $(this);
+		var mapContainer = box.find('.locsearch_box__map');
+		var searchForm = box.find('.locsearch_box__form');
+		var searchMesages = box.find('.locsearch_box__messages');
+		var addressField = searchForm.find( 'input[name=address]' );
+		var map = createMap( mapContainer[0] );
+		if( !geocoder ) {
+			geocoder = new google.maps.Geocoder();
+		}
+		
+		// Submit geocoding request
+		searchForm.on( 'submit', function(e){
+			e.preventDefault();
+			geocodeQuery( box, addressField.val() );
+		});
+		
+		// Lock/unlock box
+		box.on( 'locsearch_geocode_lock', function(){
+			$(this).data( 'isLocked', true );
+			$(this).addClass( 'locsearch_box--loading' );
+			$(this).find( ':input' ).prop( 'disabled', true );
+		});
+		box.on( 'locsearch_geocode_unlock', function(){
+			$(this).data( 'isLocked', false );
+			$(this).removeClass( 'locsearch_box--loading' );
+			$(this).find( ':input' ).prop( 'disabled', false );
+		});
+		
+		// Process geocode results
+		box.on( 'locsearch_geocode_1_result', function( e, result ){
+		});
+		box.on( 'locsearch_geocode_many_results', function( e, results ){
+			var list = $('<ul>');
+			$.each( results, function( i, result ){
+				var item = $('<li>');
+				var link = $('<a>',{ href: '#', text: result.formatted_address });
+				link.on( 'click', function(e){
+					e.preventDefault();
+				});
+				list.append( item.append( link ) );
+			});
+			searchMesages.html( '<div>'+locsearch.text.did_you_mean+'</div>' );
+			searchMesages.append( list );
+		});
+		
 	});
 });
--- inc/frontend/class.search-map.php
+++ inc/frontend/class.search-map.php
@@ -38,6 +38,7 @@ class Search_Map {
 	private function __construct() {
 		add_action( 'wp_enqueue_scripts', [$this, 'load_assets'] );
 		add_shortcode( 'locations_map', [$this, 'locations_map'] );
+		add_shortcode( 'locations_map_search', [$this, 'locations_map_search'] );
 	}
 	
 	/**
@@ -52,12 +53,23 @@ class Search_Map {
 		wp_enqueue_script( 'markerclusterer', NS\PLUGIN_URL.'assets/vendor/marker-clusterer/markerclusterer.js', [], NS\PLUGIN_VERSION );
 		wp_enqueue_script( NS\PLUGIN_NAME.'_search-map', NS\PLUGIN_URL.'assets/js/search-map.js', [NS\PLUGIN_NAME.'_google-maps-api', 'markerclusterer', 'jquery'], NS\PLUGIN_VERSION );
 		$js_data = [
-			'ajax_url'                  => admin_url( 'admin-ajax.php' ),
-			'map_attributes'            => [
-				'styles'      => json_decode( $settings['map_styles'] ),
-				'initial_lat' => floatVal( -33.865 ),
-				'initial_lng' => floatVal( 151.2094 ),
-				'max_zoom'    => absint( 15 ),
+			'ajax_url'            => admin_url( 'admin-ajax.php' ),
+			'map_attributes'      => [
+				'styles'          => json_decode( $settings['map_styles'] ),
+				'initial_lat'     => floatVal( -33.865 ),
+				'initial_lng'     => floatVal( 151.2094 ),
+				'max_zoom'        => absint( 15 ),
+			],
+			'alerts'              => [
+				'api_unavailable' => __( 'The Google Maps API is unavailable at the moment, try again later', 'locations-search' ),
+				'empty_address'   => __( 'Please enter an address', 'locations-search' ),
+				'invalid_request' => __( 'Invalid request, please verify that the address you entered is correct', 'locations-search' ),
+				'query_limit'     => __( 'You have exceeded the maximum number of allowed queries, please wait some time before trying again', 'locations-search' ),
+				'no_results'      => __( 'No results found, please try with a different address', 'locations-search' ),
+				'unknown_error'   => __( 'There was an unknown error, please try again', 'locations-search' ),
+			],
+			'text'                => [
+				'did_you_mean'    => __( 'Did you mean:', 'locations-search' ),
 			],
 		];
 		wp_localize_script( NS\PLUGIN_NAME.'_search-map', 'locsearch', $js_data );
@@ -90,6 +102,39 @@ class Search_Map {
 			'<div id="locsearch_map_%s" class="locsearch_map" data-locations="%s"></div>',
 			esc_attr( $counter ),
 			esc_attr( json_encode( $locations ) )
+		);
+		return $html;
+	}
+	
+	/**
+	 * Generates a search box to find and display locations on a map
+	 * 
+	 * @param array $atts
+	 * @param string $content
+	 * @return string
+	 */
+	public function locations_map_search( $atts=[], $content='' ) {
+		
+		// Increase counter
+		static $counter = 0;
+		$counter++;
+		
+		// Parse attributes
+		$default_atts = [
+		];
+		$atts = shortcode_atts( $default_atts, $atts, 'locations_map_search' );
+		
+		// Generate markup
+		$html = sprintf(
+			'<div id="locsearch_box_%s" class="locsearch_box">
+				<form class="locsearch_box__form">
+					<input type="text" name="address" value="" placeholder="Address">
+					<button>Search</button>
+				</form>
+				<div class="locsearch_box__messages"></div>
+				<div class="locsearch_box__map"></div>
+			</div>',
+			$counter
 		);
 		return $html;
 	}
