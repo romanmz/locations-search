7f38ddd289978f6d479bba219e7d45c493cf26d3
Román Martínez <roman.mx3@gmail.com>
2019-02-16 17:21:23 -0600

    Added uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Uninstaller Script
+ * 
+ * @version 1.0.0
+ * @since 1.0.0
+ * @package Locations_Search\Core
+ */
+
+// Exit if accessed directly
+if( !defined( 'WP_UNINSTALL_PLUGIN' ) ) exit;
+
+// delete created options
+// delete created database tables
